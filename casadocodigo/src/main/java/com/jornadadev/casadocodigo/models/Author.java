a3428d86b1e5bd5cec74f78fadefb30d1af329be
bianavic <bianavic@gmail.com>
2021-04-07 21:08:49 -0300

    inserted 2 other requisits, id and instant

--- casadocodigo/src/main/java/com/jornadadev/casadocodigo/models/Author.java
+++ casadocodigo/src/main/java/com/jornadadev/casadocodigo/models/Author.java
@@ -1,14 +1,24 @@
 package com.jornadadev.casadocodigo.models;
 
+import java.time.LocalDateTime;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
+@Entity
 public class Author {
 
+  @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long Id;
   private @NotBlank String authorName;
   public @NotBlank @Email String email;
   public  @NotBlank @Size(max = 400) String description;
+  private LocalDateTime instantOfCriation = LocalDateTime.now();
+
 
   public Author(@NotBlank String authorName, @NotBlank @Email String email,
       @NotBlank @Size(max = 400) String description) {
@@ -20,9 +30,11 @@ public class Author {
   @Override
   public String toString() {
     return "Author{" +
-        "authorName='" + authorName + '\'' +
+        "Id=" + Id +
+        ", authorName='" + authorName + '\'' +
         ", email='" + email + '\'' +
         ", description='" + description + '\'' +
+        ", instantOfCriation=" + instantOfCriation +
         '}';
   }
 }
