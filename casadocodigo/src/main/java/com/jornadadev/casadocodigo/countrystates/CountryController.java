2a132c26ea773db87026c54f21d62b02e08bdea8
bianavic <bianavic@gmail.com>
2021-04-28 21:45:46 -0300

    refactoring

--- casadocodigo/src/main/java/com/jornadadev/casadocodigo/countrystates/CountryController.java
+++ casadocodigo/src/main/java/com/jornadadev/casadocodigo/countrystates/CountryController.java
@@ -6,15 +6,17 @@ import javax.transaction.Transactional;
 import javax.validation.Valid;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class CountryController {
 
   @PersistenceContext
   private EntityManager manager;
 
-  @PostMapping(value = "country")
+  @PostMapping(value = "/countries")
   @Transactional
   public String newCountry(@RequestBody @Valid NewCountryRequest request) {
     Country newCountry = new Country(request.getCountryName());
--- casadocodigo/src/main/java/com/jornadadev/casadocodigo/countrystates/StateController.java
+++ casadocodigo/src/main/java/com/jornadadev/casadocodigo/countrystates/StateController.java
@@ -6,15 +6,17 @@ import javax.transaction.Transactional;
 import javax.validation.Valid;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class StateController {
 
   @PersistenceContext
   private EntityManager manager;
 
-  @PostMapping(value = "state")
+  @PostMapping(value = "/states")
   @Transactional
   public String newState(@RequestBody @Valid NewStateRequest request) {
     State newState = request.toModel(manager);
