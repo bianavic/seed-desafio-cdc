b888704d35269c6ba705af8e82460b73ab44f364
bianavic <bianavic@gmail.com>
2021-05-01 19:14:17 -0300

    create a purchase and tested endpoint

--- casadocodigo/src/main/java/com/jornadadev/casadocodigo/purchase/NewPurchaseRequest.java
+++ casadocodigo/src/main/java/com/jornadadev/casadocodigo/purchase/NewPurchaseRequest.java
@@ -8,6 +8,9 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import org.hibernate.validator.constraints.br.CNPJ;
 import org.hibernate.validator.constraints.br.CPF;
+import org.hibernate.validator.internal.constraintvalidators.hv.br.CNPJValidator;
+import org.hibernate.validator.internal.constraintvalidators.hv.br.CPFValidator;
+import org.springframework.util.Assert;
 
 /**
  * documento(cpf/cnpj) obrigatório e só precisa ser um cpf ou cnpj
@@ -24,10 +27,8 @@ public class NewPurchaseRequest {
   private String name;
   @NotBlank
   private String surname;
-  @CPF
-  private String cpf;
-  @CNPJ
-  private String cnpj;
+  @NotBlank
+  private String cpfOrCnpj;
   @NotBlank
   private String complement;
   @NotBlank
@@ -43,14 +44,12 @@ public class NewPurchaseRequest {
   @NotBlank
   private String cep;
 
-  public NewPurchaseRequest(String email, String name, String surname, String cpf, String cnpj,
-      String complement, String address, String city, Long countryId, String stateId,
-      String phone, String cep) {
+  public NewPurchaseRequest(String email, String name, String surname, String cpfOrCnpj, String complement,
+                            String address, String city, Long countryId, String stateId, String phone, String cep) {
     this.email = email;
     this.name = name;
     this.surname = surname;
-    this.cpf = cpf;
-    this.cnpj = cnpj;
+    this.cpfOrCnpj = cpfOrCnpj;
     this.complement = complement;
     this.address = address;
     this.city = city;
@@ -60,4 +59,44 @@ public class NewPurchaseRequest {
     this.cep = cep;
   }
 
+  public String getCpfOrCnpj() {
+    return cpfOrCnpj;
+  }
+
+  public boolean cpfOrCnpjIsValid() {
+    Assert.hasLength(cpfOrCnpj, "you shouldn't have to validate this document if it wasn't filled"); // bug
+
+    CPFValidator cpfValidator = new CPFValidator();
+    cpfValidator.initialize(null); // ant pattern
+
+    CNPJValidator cnpjValidator = new CNPJValidator();
+    cnpjValidator.initialize(null); // ant pattern
+
+    return cpfValidator.isValid(cpfOrCnpj, null) || cnpjValidator.isValid(cpfOrCnpj, null);
+  }
+
+  public Long getCountryId() {
+    return countryId;
+  }
+
+  public String getStateId() {
+    return stateId;
+  }
+
+  @Override
+  public String toString() {
+    return "NewPurchaseRequest{" +
+            "email='" + email + '\'' +
+            ", name='" + name + '\'' +
+            ", surname='" + surname + '\'' +
+            ", cpfOrCnpj='" + cpfOrCnpj + '\'' +
+            ", complement='" + complement + '\'' +
+            ", address='" + address + '\'' +
+            ", city='" + city + '\'' +
+            ", countryId=" + countryId +
+            ", stateId='" + stateId + '\'' +
+            ", phone='" + phone + '\'' +
+            ", cep='" + cep + '\'' +
+            '}';
+  }
 }
--- casadocodigo/src/main/java/com/jornadadev/casadocodigo/purchase/PurchaseController.java
+++ casadocodigo/src/main/java/com/jornadadev/casadocodigo/purchase/PurchaseController.java
@@ -1,12 +1,27 @@
 package com.jornadadev.casadocodigo.purchase;
 
 import javax.validation.Valid;
+
+import com.jornadadev.casadocodigo.validation.StateBelongsToCountryValidator;
+import com.jornadadev.casadocodigo.validation.VerifyCpfCnpjValidator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController("/api")
 public class PurchaseController {
+
+  @Autowired
+  private StateBelongsToCountryValidator stateBelongsToCountryValidator;
+
+  // validar documentos
+  @InitBinder
+  public void init(WebDataBinder binder) {
+    binder.addValidators( new VerifyCpfCnpjValidator(), stateBelongsToCountryValidator);
+  }
 
   @PostMapping(value = "/purchases")
   public String createPurchase(@RequestBody @Valid NewPurchaseRequest request) {
