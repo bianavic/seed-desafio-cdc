0d80381f14e4b1c9b0ceab4a754f1a9bf4edabac
bianavic <bianavic@gmail.com>
2021-06-13 17:31:09 -0300

    changed repository type

--- casadocodigo/src/main/java/com/jornadadev/casadocodigo/category/CategoryRepository.java
+++ casadocodigo/src/main/java/com/jornadadev/casadocodigo/category/CategoryRepository.java
@@ -1,12 +1,13 @@
 package com.jornadadev.casadocodigo.category;
 
-import com.jornadadev.casadocodigo.category.Category;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CategoryRepository extends JpaRepository<Category, String> {
+public interface CategoryRepository extends JpaRepository<Category, Long> {
 
   Optional<Category> findByCategoryName(String categoryName);
+  Optional<Category> findById(Long id);
+
 }
